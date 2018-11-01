01af54ec5300126243d5eea4e1eed3cc519ab4ab
Zoe Robertson <zoe.robertson.m@gmail.com>
2018-10-31 19:22:18 -0700

    feat: added img fetch to DetailedItem

--- src/components/DetailedItem.js
+++ src/components/DetailedItem.js
@@ -8,11 +8,20 @@ const activeStyles = StyleSheet.create({
     },
     activeHeader: {
         margin: '0',
-        fontSize: '1.7rem'
+        fontSize: '1.7rem',
+        textAlign: 'center'
     },
     activeDetails: {
         fontSize: '1.5rem',
         margin: '0'
+    },
+    imageWrapper: {
+        width: '100%',
+        maxHeight: '300px',
+        overflow: 'hidden'
+    },
+    image: {
+        width: '100%',
     }
 });
 
@@ -21,6 +30,9 @@ const DetailedItem = ({data, isLargeScreen}) => {
     return (
         <li className={css(activeStyles.activeItem)}>
             <h2 className={css(activeStyles.activeHeader)}>{name}</h2>
+            <div className={css(activeStyles.imageWrapper)}>
+                <img src={data.image_url} className={css(activeStyles.image)}/>
+            </div>
             <p className={css(activeStyles.activeDetails)}>Phone: {phone !== '' ? phone : 'No phone number available'}</p>
         </li>
     )
